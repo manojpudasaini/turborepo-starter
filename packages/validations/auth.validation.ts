52a010fe2b9c38cf28a16dbdaf41b01e5f3fba42
manojpudasaini <mpudasaini17@gmail.com>
2024-01-05 12:11:02 +0545

    update: login validation schema refactor

--- packages/validations/auth.validation.ts
+++ packages/validations/auth.validation.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email("Please enter a valid email"),
-  password: z.string().min(1, { message: "Please enter a valid password" }),
+  password: z.string().min(1, { message: "Please enter password" }),
 });
